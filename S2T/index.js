35090adc53cb5b8a2e0d8bc1ee1d706ae72d2480
tien vu duy <zombiesvip@gmail.com>
2018-10-16 15:54:41 +0700

    mainn func - move sync

--- S2T/index.js
+++ S2T/index.js
@@ -5,9 +5,9 @@ const src = "../audio_source/";
 const dest = "../done/";
 const notSupported = "../notSupported/";
 const googleApi = require('./googleapi/index.js');
-var watcher = chokidar.watch(src, {persistent: true});
+var watcher = chokidar.watch(src, {persistent: true, interval: 0, usePolling: true});
 var srcFileArr = [];
-watcher.on('add', (srcPath) => {
+watcher.on('add', async (srcPath) => {
   const srcPathSplit = srcPath.split('\\');
   const fileName = srcPathSplit[srcPathSplit.length - 1];
 
@@ -20,7 +20,14 @@ watcher.on('add', (srcPath) => {
     // main function
     fs.moveSync(srcPath, dest + fileName, {overwrite: true});
     console.log("File has been moved:", srcPath);
-    // googleApi(dest + fileName);
+    googleApi(dest + fileName);
+    
+    // main function
+    // fs.move(srcPath, dest + fileName, { overwrite: true }, err => {
+    //   if (err) return console.log("Move file failed: ", err);
+    //   console.log("File has been moved:", srcPath);
+    //   // googleApi(dest + fileName);
+    // })
   }
   else {
     fs.moveSync(srcPath, notSupported + fileName, { overwrite: true });
