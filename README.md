abb94c2e41e32dc5357c79561bdc0cfa68810051
tien vu duy <zombiesvip@gmail.com>
2018-10-17 10:37:44 +0700

    es5 - backup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chạy lệnh này trong cmd trước khi start
+set GOOGLE_APPLICATION_CREDENTIALS=E:\S2T\key.json
--- S2T/index.js
+++ S2T/index.js
@@ -1,40 +1,40 @@
 const fs = require("fs-extra");
-var chokidar = require("chokidar");
+const googleApi = require('./googleapi/index.js');
+// var chokidar = require("chokidar");
 
 const src = "../audio_source/";
 const dest = "../done/";
 const notSupported = "../notSupported/";
-const googleApi = require('./googleapi/index.js');
-var watcher = chokidar.watch(src, {persistent: true, interval: 0, usePolling: true});
-var srcFileArr = [];
-watcher.on('add', async (srcPath) => {
+// var watcher = chokidar.watch(src, {persistent: true, interval: 0, usePolling: true});
+// var srcFileArr = [];
+// watcher.on('add', async (srcPath) => callApi(srcPath))
+
+// while (true) {
+//   setTimeout(callApi())
+// }
+
+async function callApi(srcPath) {
   const srcPathSplit = srcPath.split('\\');
   const fileName = srcPathSplit[srcPathSplit.length - 1];
 
-  // srcFileArr.push({ path: srcPath, fileName });
-
-  // check file name 
+  // check file extension
   const extArr = fileName.split('.');
   const ext = extArr[extArr.length - 1];   // get extension
   if (ext === 'wav' || ext === 'mp3') {
     // main function
-    fs.moveSync(srcPath, dest + fileName, {overwrite: true});
-    console.log("File has been moved:", srcPath);
-    googleApi(dest + fileName);
+    // fs.moveSync(srcPath, dest + fileName, {overwrite: true});
+    // console.log("File has been moved:", srcPath);
+    // googleApi(dest + fileName);
     
     // main function
-    // fs.move(srcPath, dest + fileName, { overwrite: true }, err => {
-    //   if (err) return console.log("Move file failed: ", err);
-    //   console.log("File has been moved:", srcPath);
-    //   // googleApi(dest + fileName);
-    // })
+    fs.move(srcPath, dest + fileName, { overwrite: true }, err => {
+      if (err) return console.log("Move file failed: ", err);
+      console.log("File has been moved:", srcPath);
+      // googleApi(dest + fileName);
+    })
   }
   else {
     fs.moveSync(srcPath, notSupported + fileName, { overwrite: true });
     console.log("This file type is not supported!");
   }
-})
-
-// async function callApi(path, fileName) {
-
-// }
+}
